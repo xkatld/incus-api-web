1c06b4e3968cce2d7613f412cafd2a186f5c1b15
BulaSete <36999228+xkatld@users.noreply.github.com>
2025-07-02 09:06:30 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, '../dist'), // 打包到项目根目录下的 dist
+    emptyOutDir: true,
+    assetsDir: 'assets',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'ws://127.0.0.1:5000',
+        ws: true,
+      },
+    },
+  },
+})
